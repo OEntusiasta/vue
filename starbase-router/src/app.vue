25802ae557f899059b14e2eba3bab5efc7ef90ee
OEntusiasta <89933774+OEntusiasta@users.noreply.github.com>
2021-11-26 10:14:26 -0300

    starbase 2

---
+++ starbase-router/src/app.vue
@@ -0,0 +1,11 @@
+<template>
+<div id="app">
+    <h3>StarBase Router</h3>
+    <h4>
+    <router-link class="link" to="/data/people">Personagens</router-link> | 
+    <router-link class="link"  to="/data/planets">Planetas</router-link> 
+    </h4>
+    <router-view></router-view>
+</div>
+
+</template>
---
+++ starbase-router/src/components/item.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="">
+{{item}}    
+</div>
+
+
+</template>
+
+<script>
+export default {
+    props: ['item']
+    
+}
+</script>
